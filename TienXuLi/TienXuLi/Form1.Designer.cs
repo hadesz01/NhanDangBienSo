b39f7a1ddf9f819416910e718f9b3e0140dbe66d
Quang Nguyen <thanhquang.97bg@gmail.com>
2018-11-09 09:52:28 +0700

    v1

---
+++ TienXuLi/TienXuLi/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace TienXuLi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.imageBox4 = new Emgu.CV.UI.ImageBox();
+            this.imageBox5 = new Emgu.CV.UI.ImageBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(39, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(258, 228);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(312, 55);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(258, 228);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(589, 55);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(258, 228);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.Location = new System.Drawing.Point(39, 312);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(258, 228);
+            this.imageBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox3.TabIndex = 4;
+            this.imageBox3.TabStop = false;
+            // 
+            // imageBox4
+            // 
+            this.imageBox4.Location = new System.Drawing.Point(312, 312);
+            this.imageBox4.Name = "imageBox4";
+            this.imageBox4.Size = new System.Drawing.Size(258, 228);
+            this.imageBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox4.TabIndex = 5;
+            this.imageBox4.TabStop = false;
+            // 
+            // imageBox5
+            // 
+            this.imageBox5.Location = new System.Drawing.Point(589, 312);
+            this.imageBox5.Name = "imageBox5";
+            this.imageBox5.Size = new System.Drawing.Size(258, 228);
+            this.imageBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox5.TabIndex = 6;
+            this.imageBox5.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(143, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Ảnh gốc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(414, 286);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Ảnh xám";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(696, 286);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "morphology";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(143, 543);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "normalize";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(414, 543);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Đen trắng";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(696, 543);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Ảnh đích";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(245, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Tìm biển";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(443, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Biển số";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(344, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Tách kí tự";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 561);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imageBox5);
+            this.Controls.Add(this.imageBox4);
+            this.Controls.Add(this.imageBox3);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.ImageBox imageBox3;
+        private Emgu.CV.UI.ImageBox imageBox4;
+        private Emgu.CV.UI.ImageBox imageBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ TienXuLi/TienXuLi/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Tesseract;
+
+namespace TienXuLi
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> _src;
+        Image<Bgr, byte> src;
+        Image<Gray, byte> _grayImg;
+        Image<Gray, byte> dst;
+        Image<Gray, byte> temp;
+        Image<Gray, byte> temp1;
+        Image<Gray, byte> temp2;
+        Rectangle location;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                _src = new Image<Bgr, byte>(dlg.FileName);
+                Image<Bgr, byte> _src1 = new Image<Bgr, byte>(dlg.FileName);
+                src = new Image<Bgr, byte>(dlg.FileName);
+                pictureBox1.Image = _src1.Bitmap;
+            }
+            _grayImg = new Image<Gray, byte>(_src.Width, _src.Height, new Gray(0));
+            _grayImg = _src.Convert<Gray, byte>();
+
+            #region test cmt
+            //imageBox1.Image = _grayImg;
+            ////Image<Gray, byte> bw = _grayImg.Convert<Gray, byte>().ThresholdBinary(new Gray(100), new Gray(255));
+            //Image<Gray, byte> clone = _grayImg.Clone();
+            //Mat kernel=CvInvoke.GetStructuringElement(Emgu.CV.CvEnum.ElementShape.Rectangle,new Size(5,5),new Point(-1,-1));
+            //Image<Gray, byte> black =clone.MorphologyEx(Emgu.CV.CvEnum.MorphOp.Blackhat, kernel, new Point(-1, -1), 1, Emgu.CV.CvEnum.BorderType.Default, new MCvScalar(1.0));
+            //Image<Gray, float> sobel = new Image<Gray, float>(clone.Width, clone.Height, new Gray(0));
+            //sobel = clone.Laplace(9);
+            //imageBox2.Image = sobel;
+            //imageBox3.Image=black;
+            //Image<Gray, byte> normal = black.Clone();normal._EqualizeHist();
+            //imageBox5.Image = normal;
+            //Image<Gray, byte> pydown = clone.PyrDown();
+
+            //imageBox4.Image = pydown;
+            #endregion
+            Image<Gray, byte> imgClone = _grayImg.Clone();
+            Image<Gray, byte> pydown = new Image<Gray, byte>(imgClone.Width / 2, imgClone.Height / 2, new Gray(0));
+            Image<Gray, byte> nomalize = new Image<Gray, byte>(imgClone.Width / 2, imgClone.Height / 2, new Gray(0));
+            Image<Gray, byte> thresh = new Image<Gray, byte>(imgClone.Width / 2, imgClone.Height / 2, new Gray(0));
+            pictureBox1.Image = _grayImg.Bitmap;
+
+
+            Mat kernel = CvInvoke.GetStructuringElement(Emgu.CV.CvEnum.ElementShape.Rectangle, new Size(6, 3), new Point(1, 1));
+            pydown = imgClone.Clone();
+            //CvInvoke.PyrDown(pydown, pydown, Emgu.CV.CvEnum.BorderType.Default);
+            //pydown.MorphologyEx(Emgu.CV.CvEnum.MorphOp.Blackhat,kernel,new Point(-1,-1),1,Emgu.CV.CvEnum.BorderType.Default,new MCvScalar(1.0));
+
+
+            CvInvoke.MorphologyEx(pydown, pydown, Emgu.CV.CvEnum.MorphOp.Blackhat, kernel, new Point(1, 0), 1, Emgu.CV.CvEnum.BorderType.Default, new MCvScalar(1.0));
+            //giản lược chi tiết ảnh
+
+            nomalize = new Image<Gray, byte>(pydown.Width, pydown.Height, new Gray(0));
+            CvInvoke.Normalize(pydown, nomalize, 255, 0, Emgu.CV.CvEnum.NormType.MinMax);
+
+
+            thresh = nomalize.Clone();
+            //
+            CvInvoke.Threshold(thresh, thresh, 500, 255, Emgu.CV.CvEnum.ThresholdType.Otsu);
+            dst = new Image<Gray, byte>(_grayImg.Width, _grayImg.Height);
+            dst.SetZero();
+
+            //dùng ma trận điểm gồm 4 ma trận con trượt trên ảnh xét số điểm trắng lớn hơn ngưỡng nhất định
+            //xác định các vùng nghi ngờ là biển số
+            int count;
+            int diem1, diem2, diem3, diem4;
+            Rectangle rect;
+            for (int i = 0; i < thresh.Width - 16; i += 4)
+            {
+                for (int j = 0; j < thresh.Height - 32; j += 4)
+                {
+                    rect = new Rectangle(i, j, 16, 8);
+                    CvInvoke.cvSetImageROI(thresh, rect);
+                    diem1 = CvInvoke.CountNonZero(thresh);
+                    CvInvoke.cvResetImageROI(thresh);
+
+                    rect = new Rectangle(i + 16, j, 16, 8);
+                    CvInvoke.cvSetImageROI(thresh, rect);
+                    diem2 = CvInvoke.CountNonZero(thresh);
+                    CvInvoke.cvResetImageROI(thresh);
+
+                    rect = new Rectangle(i, j + 8, 16, 8);
+                    CvInvoke.cvSetImageROI(thresh, rect);
+                    diem3 = CvInvoke.CountNonZero(thresh);
+                    CvInvoke.cvResetImageROI(thresh);
+
+                    rect = new Rectangle(i + 8, j + 16, 16, 8);
+                    CvInvoke.cvSetImageROI(thresh, rect);
+                    diem4 = CvInvoke.CountNonZero(thresh);
+                    CvInvoke.cvResetImageROI(thresh);
+                    count = 0;
+
+                    if (diem1 > 15) count++;
+                    if (diem2 > 15) count++;
+                    if (diem3 > 15) count++;
+                    if (diem4 > 15) count++;
+                    if (count > 2)
+                    {
+                        rect = new Rectangle(i, j, 32, 16);
+                        CvInvoke.cvSetImageROI(thresh, rect);
+                        CvInvoke.cvSetImageROI(dst, rect);
+                        CvInvoke.cvCopy(thresh, dst, IntPtr.Zero);// copy ảnh nhị phân vào ảnh đích với ROI là i j 
+                        CvInvoke.cvResetImageROI(dst);
+                        CvInvoke.cvResetImageROI(thresh);
+                    }
+                }
+            }
+
+            temp = dst.Clone();
+            temp = temp.Dilate(4); temp = temp.Erode(9);
+
+
+            imageBox1.Image = imgClone;
+            imageBox2.Image = pydown;
+            imageBox3.Image = nomalize;
+            imageBox4.Image = thresh;
+            imageBox5.Image = temp;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Image<Gray, byte> blackimg = _src.Convert<Gray, byte>();
+            VectorOfVectorOfPoint vect = new VectorOfVectorOfPoint();
+            Mat hier = new Mat();
+            CvInvoke.FindContours(temp, vect, hier, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+            CvInvoke.DrawContours(blackimg, vect, -1, new MCvScalar(255, 255, 0));
+
+            Dictionary<int, double> dict = new Dictionary<int, double>();
+            if (vect.Size > 0)
+            {
+                for (int i = 0; i < vect.Size; i++)
+                {
+                    double area = CvInvoke.ContourArea(vect[i]);
+                    dict.Add(i, area);
+
+                }
+            }
+            var item = dict.OrderByDescending(v => v.Value);
+            foreach (var it in item)
+            {
+                int key = int.Parse(it.Key.ToString());
+                Rectangle rect = CvInvoke.BoundingRectangle(vect[key]);
+                rect.Width = rect.Width + 20;
+                rect.Height = rect.Height + 10;
+                if (rect.Height / rect.Width <= 1.2 && rect.Height / rect.Width >= 0.3)
+                {
+                    CvInvoke.Rectangle(_src, rect, new MCvScalar(100, 0, 0), 4);
+                    CvInvoke.cvSetImageROI(_grayImg, rect);
+                    temp = _grayImg;
+                    CvInvoke.Threshold(_grayImg, temp, 500, 255, Emgu.CV.CvEnum.ThresholdType.Otsu);
+                    CvInvoke.Resize(temp, temp, new Size(640, 480));
+                    imageBox1.Image = temp;
+                    //MessageBox.Show(temp.Height.ToString() + temp.Width.ToString());
+                    location = rect;
+                    
+                }
+                CvInvoke.Rectangle(_src, rect, new MCvScalar(0, 0, 255), 3);
+                
+            }
+         
+            pictureBox1.Image = _src.Bitmap;
+            label1.Text = "contours";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string path = "d:\\Untitled.png";
+            Image<Bgr, byte> iii = new Image<Bgr, byte>(path);
+            Image<Gray, byte> ii = iii.Convert<Gray, byte>();
+            //Image<Gray, byte> ii = temp1;
+            TesseractEngine engine = new TesseractEngine("./tessdata", "eng");
+            Page pag = engine.Process(ii.Bitmap, PageSegMode.Auto);
+            string text = pag.GetText();
+            MessageBox.Show(text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Image<Bgr, byte> color1 = new Image<Bgr,byte>(temp.Width,temp.Height);
+            color1.SetZero();
+            CvInvoke.cvSetImageROI(src, location);
+            CvInvoke.cvCopy(src, color1, IntPtr.Zero);
+            Mat kernel=CvInvoke.GetStructuringElement(Emgu.CV.CvEnum.ElementShape.Cross,new Size(4,4),new Point(-1,-1));
+            temp1 = temp.Clone();
+            CvInvoke.Erode(temp, temp1, kernel, new Point(-1, -1), 1, Emgu.CV.CvEnum.BorderType.Default, default(MCvScalar));
+            //CvInvoke.Dilate(temp, temp2, null, new Point(-1, -1), 1, Emgu.CV.CvEnum.BorderType.Default, default(MCvScalar));
+            temp2 = temp1.Clone();
+            temp2._Dilate(1);
+            temp2._Not();
+            imageBox4.Image = temp2;
+            imageBox5.Image = temp1;
+
+            //Tìm contours của số trên ảnh temp2//
+
+            Image<Gray,byte>temp3=temp2.Clone();
+            VectorOfVectorOfPoint vect = new VectorOfVectorOfPoint();
+            Mat hier = new Mat();
+            CvInvoke.FindContours(temp3, vect, hier, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+            CvInvoke.DrawContours(temp3, vect, -1, new MCvScalar(255, 255, 0));
+
+            Dictionary<int, double> dict = new Dictionary<int, double>();
+            if (vect.Size > 0)
+            {
+                for (int i = 0; i < vect.Size; i++)
+                {
+                    double area = CvInvoke.ContourArea(vect[i]);
+                    dict.Add(i, area);
+                }
+            }
+            var item = dict.OrderByDescending(v => v.Value);
+            foreach (var it in item)
+            {
+                int key = int.Parse(it.Key.ToString());
+                Rectangle rect = CvInvoke.BoundingRectangle(vect[key]);
+                CvInvoke.Rectangle(temp3, rect, new MCvScalar(255, 0, 0), 1);               
+                CvInvoke.Rectangle(color1, rect, new MCvScalar(0, 0, 255), 1);
+            }
+            CvInvoke.cvResetImageROI(src);
+            imageBox2.Image = color1;
+            imageBox3.Image = temp3;
+        }
+    }
+}
